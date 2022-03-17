0d716f1fe45b9380f9d28982e189c218fb836528
IT21297304 <99237579+IT21297304@users.noreply.github.com>
2022-03-17 19:05:07 +0000

    My work is over

--- exercise04.cpp
+++ exercise04.cpp
@@ -1,21 +1,31 @@
-#include <iostream>
+#include<iostream>
+
 using namespace std;
 
 void print(int len, int wth);
-void input(int len, int wth);
+void input(int &len, int &wth);
 
-// Do not change the main() function
-int main() {
-   int length = 10, width = 5;
-   input(length, width);
-   print(length, width);
-   return 0;
+int main(){
+	
+	int length = 10, width = 5;
+	
+	input(length, width);
+	print(length,width);
+	return 0;
+	
 }
 
-// Do not change the print() function
-void print(int len, int wth) {
-   cout << "Length : " << len 
-        << ", Width  : " << wth << endl;
+void print (int len, int wth){
+	
+	cout<<"Length : "<<len
+		<<" , Width : "<<wth <<endl;
 }
 
+void input(int &len, int &wth){
+	
+	cout<<"Enter box width : ";
+	cin>>wth;
+	cout<<"Enter box length : ";
+	cin>>len;
+}
 // Implement the Input Function here
